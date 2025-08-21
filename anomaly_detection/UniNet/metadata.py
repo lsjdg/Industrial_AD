d5c758bfb4dc596bb2d51c1b6c492d3cd97e8368
lsjdg <jjdg148@gmail.com>
2025-08-21 11:35:29 +0900

    feat: log metadata

---
+++ anomaly_detection/UniNet/metadata.py
@@ -0,0 +1,28 @@
+# domain
+industrial = [
+    "MVTecAD",
+]
+
+# training method
+unsupervised = [
+    "MVTecAD",
+]
+
+# classes
+mvtec_list = [
+    "carpet",
+    "bottle",
+    "hazelnut",
+    "leather",
+    "cable",
+    "capsule",
+    "grid",
+    "pill",
+    "transistor",
+    "metal_nut",
+    "screw",
+    "toothbrush",
+    "zipper",
+    "tile",
+    "wood",
+]
